e6e05b8bd3e0410bb13e073b87473ec3e0d6c4ee
ALONE <www.mohamadmobin1385@gmail.com>
2025-09-30 14:20:57 +0330

    update package.json

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import homepage from '../views/homepage.vue'
 import createAcc from '@/views/createAcc.vue'
@@ -47,7 +47,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/NFT-marketplace/'),
   routes,
     scrollBehavior() {
     return { 
